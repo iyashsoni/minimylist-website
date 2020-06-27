510336ce617c5a787947a9de65bebe65bbc24c18
Yash Soni <yasoni12@in.ibm.com>
2020-06-28 00:23:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,158 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className="header">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
+          <img
+            aria-label="app-icon"
+            src={process.env.PUBLIC_URL + "/assets/app.png"}
+            width="100px"
+            height="100px"
+            alt="app-icon"
+          />
+          <p style={{ marginLeft: "1rem" }}>minimylist</p>
+        </div>
       </header>
+      <div
+        id="description"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          marginTop: "2rem",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ margin: "1rem" }}>
+          <h3>What is it?</h3>
+        </div>
+        <div style={{ margin: "1rem" }}>
+          <h4>Your minimal productivity assistant!</h4>
+        </div>
+        <div style={{ margin: "1rem" }}>
+          <h3>Wait, what?</h3>
+        </div>
+        <div style={{ margin: "1rem" }}>
+          <h4>
+            Well, it's a Web Extension that you can use for keeping a tab on
+            quick-notes and tasks at hand, <br />
+            so yeah - a productivity assistant!
+          </h4>
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <h3>Features?</h3>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <h4>
+                Notes
+                <span role="img" aria-label="note">
+                  📝
+                </span>
+              </h4>
+            </li>
+            <li>
+              <h4>
+                Tasks
+                <span role="img" aria-label="tick">
+                  ✅
+                </span>
+              </h4>
+            </li>
+            <li>
+              <h4>
+                Inspirational Quote of the Day
+                <span role="img" aria-label="blessed">
+                  😇
+                </span>
+              </h4>
+            </li>
+            <li>
+              <h4>
+                Dark mode{" "}
+                <span role="img" aria-label="moon">
+                  🌚
+                </span>
+                / Light mode
+                <span role="img" aria-label="sun">
+                  🌞
+                </span>
+              </h4>
+            </li>
+          </ul>
+        </div>
+        <div style={{ padding: "1rem" }}>
+          <h3>How does it look?</h3>
+        </div>
+        <img
+          width="100%"
+          style={{ padding: "0.5rem" }}
+          src={process.env.PUBLIC_URL + "/assets/screenshots/AppSS.png"}
+          alt="Light Mode Screenshot"
+          app-icon="Light Mode Screenshot"
+        />
+      </div>
+
+      <div style={{ margin: "1rem" }}>
+        <h3>Cool! Show me where can I get this</h3>
+      </div>
+      <div
+        style={{
+          margin: "1rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <a
+          href="https://addons.mozilla.org/en-US/firefox/addon/minimylist/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginRight: "1rem",
+            }}
+          >
+            <img
+              src={process.env.PUBLIC_URL + "/assets/firefox.png"}
+              alt="Firefox icon"
+              app-icon="Firefox icon"
+              style={{ height: "50px" }}
+            />
+            <h6 style={{ marginLeft: "0.5rem", fontSize: "16px" }}>
+              Available as a <br />
+              Firefox addon
+            </h6>
+          </div>
+        </a>
+        <a
+          href="https://chrome.google.com/webstore/detail/minimylist/kjopgffinilifnhhjflchcgcbfdjjpea"
+          target="_blank"
+          rel="noreferrer noopener"
+          style={{ alignItems: "center", display: "flex" }}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "/assets/chrome.png"}
+            style={{ marginLeft: "1rem", height: "70px" }}
+            app-icon="Chrome icon"
+            alt="Chrome icon"
+          />
+        </a>
+      </div>
     </div>
   );
 }
